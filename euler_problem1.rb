5a6477f18b5ec017166ab42ecb92c1b633438272
Naoya SASAHARA <sasahara@example.com>
2017-06-09 07:38:18 +0900

    poblem1　一部修正

--- euler_problem1.rb
+++ euler_problem1.rb
@@ -3,10 +3,8 @@ class Euler_problem1
 		(1..999).each do |num|
   			if num % 3 == 0 && num % 5 ==0
     			total += num
-  			elsif num % 3 == 0
+  			elsif num % 3 == 0 || num % 5 == 0
    			    total += num
-  			elsif num % 5 == 0
-    		    total += num
   			end
 		end 
 		puts total
