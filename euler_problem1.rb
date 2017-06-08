9593756e3a6e6824a0a8167334962e39fbac16ae
Naoya SASAHARA <sasahara@example.com>
2017-06-08 23:40:07 +0900

    problem1 変数名変更

--- euler_problem1.rb
+++ euler_problem1.rb
@@ -1,13 +1,13 @@
 class Euler_problem1
-		i = 0
-		(1..999).each do |k|
-  			if k % 3 == 0 && k % 5 ==0
-    			i += k
-  			elsif k % 3 == 0
-   			    i += k
-  			elsif k % 5 == 0
-    		    i += k
+		total = 0
+		(1..999).each do |num|
+  			if num % 3 == 0 && num % 5 ==0
+    			total += num
+  			elsif num % 3 == 0
+   			    total += num
+  			elsif num % 5 == 0
+    		    total += num
   			end
 		end 
-		puts i
+		puts total
 end
