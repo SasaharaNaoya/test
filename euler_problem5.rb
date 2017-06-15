0d1b0226eb57da615de83bec29e25e618ec5a93d
sasaharanaoya <fate.mnmn15@gmail.com>
2017-06-16 08:01:37 +0900

    euler_problem5 動きはする

---
+++ euler_problem5.rb
@@ -0,0 +1,24 @@
+# 2520 は 1 から 10 の数字の全ての整数で割り切れる数字であり, そのような数字の中では最小の値である.
+
+# では, 1 から 20 までの整数全てで割り切れる数字の中で最小の正の数はいくらになるか.
+
+class Euler_problem5
+	ans = 1
+	count = 1
+	loop{
+		a = true
+	  	(2..20).each {|num|
+	 		while !(count % num == 0) do
+	 			a = false
+	 			break
+	 		end
+	 	}
+	 	if a == true
+	 		ans = count
+	 		break
+	 	end
+	 	count += 1
+	 	p count
+	}
+	p ans
+end
