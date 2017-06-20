36d454331bd41f2fa61799e267aeabcd979cba4f
sasaharanaoya <fate.mnmn15@gmail.com>
2017-06-20 22:40:09 +0900

    Euler_problem5_kai 途中

--- euler_problem5_kai.rb
+++ euler_problem5_kai.rb
@@ -1,20 +1,26 @@
 class Euler_problem5_kai
-	def main
-		p get_min_divisible_number
+
+	def main()
+		p get_min_divisible_number(1,20)
 	end
 
 	def get_min_divisible_number(first,last)
 		result = first
-		(first..last)each{|element|
-			result lcm(result,i)
+		(first..last).each {|element|
+			result = lcm(result,element)
 		}
 		return result
-	}
+	end
 
 	def gcd(x,y)
-
+		while !(y > 0) do
+			y = x % y
+			x = y
+		end
+		return x
 	end
 
 	def lcm(x,y)
-
+		return (x * y) / gcd(x,y)
 	end
+end
