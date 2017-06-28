f43a064a1d0e7e9d3771a699daf1172b79560257
sasaharanaoya <fate.mnmn15@gmail.com>
2017-06-28 22:04:58 +0900

    euler9 upload

---
+++ euler_problem9.rb
@@ -0,0 +1,12 @@
+class Euler_problem9
+
+	(1..1000).each do |a|
+	  (a..(1000 - a)).each do |b|
+	    c = 1000 - a - b
+	    if a**2 + b**2 == c**2
+	    	puts a * b * c
+	    end
+	  end
+	end
+	
+end
