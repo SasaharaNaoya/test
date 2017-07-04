a0dbb03a8d281406a3c4c135b8440cd66e315c6f
sasaharanaoya <fate.mnmn15@gmail.com>
2017-07-04 23:31:26 +0900

    euler14 無駄多いが完成

--- euler_problem14.rb
+++ euler_problem14.rb
@@ -10,18 +10,27 @@
 class Euler_problem14
 
 	def self.length(num)
-	    while (value != 1)do
-
+	    items_length = 0
+	    value = num
+	    while (value != 1) do
+	    	if value % 2 == 0
+	    		value = value / 2
+	    		items_length += 1
+	    	else
+	    		value = value * 3 + 1
+	    		items_length += 1
+	    	end
 	    end
-
-
-	    return num if num <= 1
+	    return items_length if value <= 1
 	end
-
+	max_items = 0
+	max_num = 0
 	(1..1000000).each do |num|
-		self.length(num)
-
+		items_length = self.length(num)
+		if items_length > max_items 
+			max_items = items_length
+			max_num = num
+		end
 	end		
-
-		
+	puts max_num
 end
