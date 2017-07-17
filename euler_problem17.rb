000836706a24999210305063ffd34afaf74b942a
sasaharanaoya <fate.mnmn15@gmail.com>
2017-07-17 23:17:27 +0900

    euler17 途中

---
+++ euler_problem17.rb
@@ -0,0 +1,28 @@
+# 1~9
+def ones_place
+	[3,3,5,4,4,3,5,5,4]
+
+end
+
+# 10~19
+def tens_place
+
+end
+
+# 20~99
+
+# 100~999
+def hundreds_place
+
+end
+
+# 1000~9999
+def thousands_place
+
+end
+
+def distinguish(num)
+	if num 
+end
+
+p distinguish(5)
