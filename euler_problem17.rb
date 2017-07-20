58d0654f517dcf863fa345f6dd5ab719630fe656
sasaharanaoya <fate.mnmn15@gmail.com>
2017-07-20 23:58:46 +0900

    修正

--- euler_problem17.rb
+++ euler_problem17.rb
@@ -1,9 +1,11 @@
 # 1~9
 def ones_place(limit)
 	result = 0
+	i = 0
 	[3,3,5,4,4,3,5,5,4].each do |num|
-		if i != limit-1 #fixme
-			result = num
+		if i != limit 
+			result += num
+			i += 1
 		else
 			break
 		end
@@ -14,9 +16,11 @@ end
 # 10~19
 def tens_place(limit)
 	result = 0
+	i = 0
 	[3,6,6,8,8,7,7,9,8,8].each do |num|
-		if i != limit-1 #fixme
-			result = num
+		if i != limit+1 #fixme
+			result += num
+			i += 1
 		else
 			break
 		end
@@ -25,13 +29,17 @@ def tens_place(limit)
 end
 
 # 20~99
+def twelve_ninetynine(limit)
+	[6,6,5,5,5,7,6,6]
 
 # 100~999  (hundred and)で10文字
 def hundreds_place(limit)
 	result = 0
+	i = 0
 	[13,13,15,14,14,13,15,15,14].each do |num| 
 		if i != limit-1 #fixme
-			result = num
+			result += num + tens_place()
+			i += 1
 		else
 			break
 		end
@@ -45,8 +53,14 @@ def thousands_place
 end
 
 def distinguish(num)
-	if num < 10 #fixme
-
+	ones_place_num = num % 10 
+	if num < 10 
+		return ones_place(num)
+	elsif num < 20
+		return tens_place(ones_place_num) + 36
+	elsif num < 1000
+		return hundreds_place(num)
+	end
 end
 
-p distinguish(5)
+p distinguish(19)
