ee2271ef381bcda9089c8fcb26e6773ab6459dd2
sasaharanaoya <fate.mnmn15@gmail.com>
2017-08-15 13:46:59 +0900

    euler19 途中

--- euler_problem19.rb
+++ euler_problem19.rb
@@ -7,38 +7,53 @@
 
 # 20世紀（1901年1月1日から2000年12月31日）中に月の初めが日曜日になるのは何回あるか?
 
-def getdays(month)
+def getdays(month,year)
 	if ['4','6','9','11'].include?(month)
 		return 30
 	elsif ['1','3','5','7','8','10','12'].include?(month)
 		return 31
-	elsif month % 400 == 0
+	elsif year % 400 == 0
 		return 28
-	elsif month % 100 == 0
+	elsif year % 100 == 0
 		return 28
-	elsif month % 4 == 0
+	elsif year % 4 == 0
 		return 29
 	else
 		return 28
 	end
 end
 
-def nextdate(date,mouth,year)
-
-	
+def nextdate(date)
+	return (date[0] + getdays(date[1],date[2])) % 7
 end
-def nextmonth(month,year)
 
+def nextmonth(date)
+	if date[1] == 12
+		date[1] = 1
+		date[2] += 1 
+		return date
+	else
+		date[1] += 1
+		return date
+	end
 end
 
 def main
 	start_year = 1900
-	end_year = 2000
+	last_year = 2000
 	date = [1,1,1900]
 	ans = 0
 
-	while
+	while date[2] <= last_year
+		date[0] = nextdate(date)
+		date = nextmonth(date)
+
+		if date[0] == 0
+			ans += 1
+		end
+	end
+	return ans
 end
 
 
-p getdays('12')
+p main
